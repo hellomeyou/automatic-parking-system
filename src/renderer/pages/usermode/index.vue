4c429da4593c73ff4933505ba7e8ba26a39a8469
Aditya <171426589@.com>
2019-04-28 15:28:56 +0800

    Fixed 运动姿态算法完善

--- src/renderer/pages/usermode/index.vue
+++ src/renderer/pages/usermode/index.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
     .usermode
-        router-link.usermode(to="/") 首页
-        .title 用户模式界面
+        router-link.usermode(to="/") 校准模式
+        .title 测量模式
         .status
             span(v-html="frontStatusMessage")
         .status
-            span(v-html="sideStatusMessage")
+            span(v-html="sideLeftStatusMessage")
+            span(v-if="sideLeftStatusMessage && sideRightStatusMessage") ,
+            span(v-html="sideRightStatusMessage")
         .status
             span(v-html="stopStatusMessage")
         .usermode__main
             .car.garage(:style="{width: garage.length + 'px', height: garage.height + 'px'}")
+                .point(:style="{top: garage.height - carCenter.y + 'px', left: garage.length - carCenter.x + 'px'}")
                 .stopline(:style="{width: targetArea.length + 'px', height: targetArea.height + 'px', top: leftEndline + 'px', left: beforeEndline + 'px'}")
                 <!--.car__main(:style="{'transform': 'rotate(' + carDegree + 'deg)', bottom: carPosition.y + 'px', left: carPosition.x + 'px' }")-->
-                .car__main(:style="{'transform': 'translate('+ carPosition.x +'px, '+ (this.garage.height - carWidth - carPosition.y) +'px) rotate(' + carDegree + 'deg)'}")
+                .car__main(:style="{width: carHeight + 'px', height: carWidth + 'px', 'transform': 'translate('+ carPosition.x + 'px, ' + (garage.height - carWidth - carPosition.y) + 'px) rotate(' + carDegree + 'deg)'}")
                     .car__LeftFrontWheel.car--wheel(:style="{'transform': 'rotate(' + LeftFrontWheelDegree + 'deg)'}")
                     .car__RightFrontWheel.car--wheel(:style="{'transform': 'rotate(' + RightFrontWheelDegree + 'deg)'}")
                     .car__LeftRearWheel.car--wheel
@@ -53,13 +56,16 @@
                 wheelAngle: 0,
                 // 提醒
                 frontStatusMessage: '',
-                sideStatusMessage: '',
+                sideLeftStatusMessage: '',
+                sideRightStatusMessage: '',
                 stopStatusMessage: '',
 
                 temporaryData: {
                     x: [],
                     y: []
-                }
+                },
+
+                angleHeight: ''
             }
         },
         created () {
@@ -78,6 +84,7 @@
                         length: data.parking_x / 10,
                         height: data.parking_y / 10
                     }
+                    console.log(this.garage)
                     this.targetArea = {
                         length: data.target_x / 10,
                         height: data.target_y / 10
@@ -86,19 +93,15 @@
                     this.beforeEndline = (data.parking_x - data.target_x) / 10 / 2
                     this.leftEndline = (data.parking_y - data.target_y) / 10 / 2
                     this.rightEndline = (data.parking_y - (data.parking_y - data.target_y) / 2) / 10
+
+                    this.onVehicleAttitude()
                 } else {
                     this.$message({
                         message: args.message,
-                        type: 'success'
+                        type: 'warning'
                     })
                 }
             })
-            /*
-            *  重新请求当前车库标准
-            * */
-            if (!this.garageCenter.x && !this.garageCenter.y) {
-                ipcRenderer.send('runtime_para')
-            }
 
             ipcRenderer.on('vehicle_attitude-reply', (event, args) => {
                 // {
@@ -127,12 +130,16 @@
                     }
 
                     // 车体旋转角度后高（宽）
-                    const angkeHeight = data.car_length / 10 * Math.cos(data.car_angle / 1000) + data.car_width / 10 * Math.sin(data.car_angle / 1000)
+                    this.angleHeight = (data.car_length * Math.sin(data.car_angle / 1000 * Math.PI / 180) + data.car_width * Math.cos(data.car_angle / 1000 * Math.PI / 180)) / 10
+
                     this.carPosition = {
                         x: data.car_center.x / 10 - data.car_length / 10 / 2,
-                        y: data.car_center.y / 10 - angkeHeight / 2
+                        y: data.car_center.y / 10 - data.car_width / 10 / 2
                     }
                     console.log(this.carPosition)
+                    console.log(this.garage.height)
+                    console.log(this.angleHeight)
+                    console.log(this.carPosition.y)
                     this.LeftFrontWheelDegree = -data.left_front_wheel_angle / 1000 - -data.car_angle / 1000
                     this.RightFrontWheelDegree = -data.right_front_wheel_angle / 1000 - -data.car_angle / 1000
 
@@ -145,7 +152,6 @@
                     })
                 }
             })
-            this.onVehicleAttitude()
             setInterval(() => {
                 this.onVehicleAttitude()
             }, 100)
@@ -153,7 +159,7 @@
                 console.log(this.temporaryData)
                 if (this.onStddev(this.temporaryData.x) < 2 && this.onStddev(this.temporaryData.y) < 2) {
                     const frontWheelDegree = (Math.abs(this.LeftFrontWheelDegree) + Math.abs(this.RightFrontWheelDegree)) / 2
-                    if (this.carPosition.x - this.beforeEndline < 0 || (this.carPosition.y < this.leftEndline || (parseInt(this.carPosition.y) + parseInt(this.carWidth)) > this.rightEndline)) {
+                    if (this.carPosition.x - this.beforeEndline < 0 || (this.carCenter.y - this.angleHeight / 2 < this.leftEndline || parseInt(this.carCenter.y) + parseInt(this.angleHeight / 2) > this.rightEndline)) {
                         this.stopStatusMessage = '请将车停在停车区域内'
                     } else if (frontWheelDegree > this.wheelAngle) {
                         if (this.LeftFrontWheelDegree + this.RightFrontWheelDegree < 0) {
@@ -173,13 +179,21 @@
                 }
             }, 3000)
         },
+        mounted () {
+            /*
+            *  重新请求当前车库标准
+            * */
+            if (!this.garageCenter.x && !this.garageCenter.y) {
+                ipcRenderer.send('runtime_para')
+            }
+        },
         watch: {
             'carCenter.x': {
                 handler (newName, oldName) {
-                    this.carPosition = {
-                        ...this.carPosition,
-                        x: this.carCenter.x - this.carHeight / 2
-                    }
+                    // this.carPosition = {
+                    //     ...this.carPosition,
+                    //     x: this.carCenter.x - this.carHeight / 2
+                    // }
                     console.log(this.carPosition.x - this.beforeEndline)
                     if (this.carPosition.x - this.beforeEndline > 150) {
                         this.frontStatusMessage = `<span style="color: orange">请继续驶入</span>`
@@ -204,21 +218,28 @@
             },
             'carCenter.y': {
                 handler (newName, oldName) {
-                    this.carPosition = {
-                        ...this.carPosition,
-                        y: newName - this.carWidth / 2
-                    }
-                    const distance = parseInt(this.carPosition.y) + parseInt(this.carWidth)
+                    // this.carPosition = {
+                    //     ...this.carPosition,
+                    //     y: newName - this.carWidth / 2
+                    // }
+                    const distance = parseInt(this.carCenter.y) + parseInt(this.angleHeight / 2)
                     console.log(`距左距离:${distance}`)
                     console.log(distance - this.rightEndline)
-                    if (this.carPosition.y < this.leftEndline) {
-                        const beyondDistance = 70 - this.carPosition.y - this.leftEndline
-                        this.sideStatusMessage = `您已经超出左端线<span style="color: red">${Math.abs(parseFloat(beyondDistance / 100)).toFixed(2)}</span>米`
-                    } else if (distance > this.rightEndline) {
-                        const beyondDistance = distance - this.rightEndline
-                        this.sideStatusMessage = `您已经超出右端线<span style="color: red">${Math.abs(parseFloat(beyondDistance / 100)).toFixed(2)}</span>米`
+                    console.log(this.carCenter.y)
+                    console.log(this.angleHeight / 2)
+                    console.log(this.carCenter.y - this.angleHeight / 2)
+                    console.log(this.leftEndline)
+                    if (this.carCenter.y - this.angleHeight / 2 < this.leftEndline) {
+                        const beyondDistance = (this.carCenter.y - this.angleHeight / 2) - this.leftEndline
+                        this.sideLeftStatusMessage = `您已经超出左端线<span style="color: red">${Math.abs(parseFloat(beyondDistance / 100)).toFixed(2)}</span>米`
                     } else {
-                        this.sideStatusMessage = ''
+                        this.sideLeftStatusMessage = ''
+                    }
+                    if (distance > this.rightEndline) {
+                        const beyondDistance = distance - this.rightEndline
+                        this.sideRightStatusMessage = `您已经超出右端线<span style="color: red">${Math.abs(parseFloat(beyondDistance / 100)).toFixed(2)}</span>米`
+                    } else {
+                        this.sideRightStatusMessage = ''
                     }
                 },
                 immediate: true
@@ -310,5 +331,12 @@
                 }
             }
         }
+    }
+    .point {
+        width: 1px;
+        height: 1px;
+        background: black;
+        z-index: 100;
+        position: absolute;
     }
 </style>
